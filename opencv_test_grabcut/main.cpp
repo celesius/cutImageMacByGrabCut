cbf3956ec02ce6b64e8f25548d9717284a9a0e92
BoJiang <188166614@qq.com>
2015-09-03 10:15:29 +0800

    Initial Commit

---
+++ opencv_test_grabcut/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  opencv_test_grabcut
+//
+//  Created by vk on 15/9/3.
+//  Copyright (c) 2015年 quxiu8. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
